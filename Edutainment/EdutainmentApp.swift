fbaff36d0bd659cc4d04b55a9a4364406f125921
TheGoodAres <oprearobert0202@gmail.com>
2023-03-17 13:02:09 +0000

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Robert-Dumitru Oprea on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
